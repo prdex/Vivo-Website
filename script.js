4b2f2a38f55314ec586244d3086c8132a51bdbaf
Pratik Bambhania <bambhania.pratik9090@gmail.com>
2018-06-22 10:27:38 -0700

    New repo

---
+++ script.js
@@ -0,0 +1,35 @@
+const hideClose = () =>{
+    $('#close-nav').css({'display':'none'});
+}
+
+const showClose = () =>{
+    $('#close-nav').css({'display':'inline'});
+}
+
+$('.sidenav').sidenav({
+    menuWidth: 250,
+    closeOnClick: true,
+    draggable:true,
+    edge: 'right',
+    onOpenEnd: showClose,
+    onCloseStart: hideClose
+});
+
+$('.sidenav-trigger').click(()=>{
+    $('#close-nav').css({'display':'inline'});
+});
+
+
+$(window).scroll(function() {
+    var abc = document.getElementById('navwrap');
+    if ($(this).scrollTop() > 400) {
+        $(abc).css({
+            "background-color":"#6699ff",
+            "transition":"1s"
+        })
+    } else {
+        $(abc).css({
+        "background-color":"transparent"
+    })
+}
+});
